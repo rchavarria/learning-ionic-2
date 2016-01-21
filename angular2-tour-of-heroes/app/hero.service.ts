d7746f084673f9f587f89ddfd443858bdac4ac6c
Rubén Chavarría <txingo@gmail.com>
2016-01-21 23:52:15 +0100

    Complete Angular2 tutorials: quickstart and Tour of Heroes

---
+++ angular2-tour-of-heroes/app/hero.service.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+import {Injectable} from 'angular2/core';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+
+}
+
